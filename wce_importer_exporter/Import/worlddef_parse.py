3c33651984601e709702b195037d64912c41664a
dariusuknuis <darius_uknuis@yahoo.com>
2025-03-29 21:19:16 -0400

    Add worlddef parse

---
+++ wce_importer_exporter/Import/worlddef_parse.py
@@ -0,0 +1,23 @@
+import shlex
+
+def worlddef_parse(r, parse_property, current_line):
+    """
+    Parses the WORLDDEF and its properties.
+    """
+    records = shlex.split(current_line)
+    if records[0] != "WORLDDEF":
+        raise Exception(f"Expected WORLDDEF, got {records[0]}")
+
+    worlddef = []
+
+    # Parse NUMWORLDNODES
+    records = parse_property(r, "NEWWORLD", 1)
+    worlddef["new_world"] = int(records[1])
+
+    records = parse_property(r, "ZONE", 1)
+    worlddef["zone"] = int(records[1])
+
+    records = parse_property(r, "EQGVERSION?", 1)
+    worlddef["eqg_version"] = records[1]
+
+    return worlddef
