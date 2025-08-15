1d84eeab0ec065964b924af370b4ed2e51413934
dariusuknuis <86813118+dariusuknuis@users.noreply.github.com>
2025-08-14 21:01:08 -0400

    Update __init__.py
    Need a number value for Quail version

--- wce_importer_exporter/__init__.py
+++ wce_importer_exporter/__init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "description": "A tool to import EQ WCE files and export model data.",
     "author": "Darius",
     "version": (2, 4, 0),
-    "Quail": (dev),
+    "Quail": (1.6.0),
     "blender": (3, 6, 2),
     "location": "View3D > Tool Shelf > WCE Importer/Exporter",
     "category": "Import-Export",
@@ -293,3 +293,4 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+
